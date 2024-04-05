8de2fd3dd36f856abdf9ddabbadabda2da9e7f75
BlueandBlack12 <jamesjrpat@gmail.com>
2024-04-05 22:47:05 +0800

    add all

---
+++ HotelManagementSystem/Receptionist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementSystem
+{
+    public class Receptionist : User
+    {
+        public Receptionist(string name, string address, string email, long phoneNumber) : base(name, address, email, phoneNumber)
+        {
+        }
+
+        public void BookRoom(Guest guest, HotelRoom room, DateTime startTime, DateTime endTime)
+        {
+            guest.BookRoom(room, startTime, endTime);
+        }
+    }
+}
